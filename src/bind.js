f451e8a9260d6af649ebed48054a88fef51c224f
TeemoPeng <danielpog@163.com>
2021-03-08 09:21:42 +0800

    apply, call

--- src/bind.js
+++ src/bind.js
@@ -1,18 +1,4 @@
-Function.prototype.myBind = function(obj){
-    if (typeof this !== 'function') {
-        throw new Error('this must be function')
-    }
-    const self = this    
-    const args = Array.prototype.slice.call(arguments, 1)
-    const fBound = function(){
-        const bindArgs = Array.prototype.slice.call(arguments)
-        return self.apply(obj, args)
-    }
-    if (this.prototype) {
-        fBound.prototype = Object.create(this.prototype)
-    }
-    return fBound
-}
+
 
 const A = {
     name: 'A',
--- src/call.js
+++ src/call.js
@@ -1,12 +1,15 @@
-Function.prototype.myApply = function(obj){
+Function.prototype.myCall = function(obj){
     let context = obj || window
     context.fn = this
-    let args = arguments[1] ? arguments[1] : []
+    let args = []
+    for(let i = 1; i < arguments.length; i++){
+        args.push(arguments[i])
+    }
+
     const result = context.fn(...args)
     delete context.fn
     return result
 }
-
 const A = {
     name: 'A',
     getName: function(msg1, msg2 = ''){
@@ -18,4 +21,4 @@ const B = {
     name: 'B'
 }
 
-console.log(A.getName.myApply(B, ['hello', 'world']))
+console.log(A.getName.myCall(B, 'hello', 'world'))
